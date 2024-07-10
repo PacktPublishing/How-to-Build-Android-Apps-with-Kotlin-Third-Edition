2e9d3d0b247d35c3c1c5ecf457dd9c92a93afe70
alexforrester <aforresterwork@gmail.com>
2024-07-10 10:54:37 +0100

    Adds Exercise 1.03 with AndroidManifest Internet permission and AndroidView Composable displaying WebView

---
+++ Chapter01/Exercise1.03/app/src/main/java/com/example/jetpackcompose/myapplication/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.jetpackcompose.myapplication
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.jetpackcompose.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val webView = WebView(this)
+            webView.settings.javaScriptEnabled = true
+            webView.loadUrl("https://www.google.com")
+
+            AndroidView(
+                modifier = Modifier.fillMaxSize(),
+                factory = { context ->
+                    webView
+                })
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
