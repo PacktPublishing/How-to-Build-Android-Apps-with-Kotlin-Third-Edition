568cb66642a9a0f8903108065415bdec5fd48ead
Jomar Tigcal <jomar@tigcal.com>
2024-08-01 22:13:05 -0700

    Adding Chapter 15 Exercise 1

---
+++ Chapter15/Exercise15.01/app/src/test/java/com/example/popularmovies/MovieViewModelTest.kt
@@ -0,0 +1,43 @@
+package com.example.popularmovies
+
+import com.example.popularmovies.model.Movie
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import java.util.Calendar
+
+class MovieViewModelTest {
+    @get:Rule
+    val mockkRule = MockKRule(this)
+
+    @MockK
+    lateinit var movieRepository: MovieRepository
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Test
+    fun getPopularMovies() {
+        val movies = listOf(
+            Movie(
+                title = "Title",
+                releaseDate = Calendar.getInstance().get(Calendar.YEAR).toString()
+            )
+        )
+
+        val movieViewModel = MovieViewModel(movieRepository, testDispatcher)
+
+        runTest {
+            coEvery { movieRepository.getPopularMovies() } returns flowOf(movies)
+
+            movieViewModel.getPopularMovies()
+            testDispatcher.scheduler.advanceUntilIdle()
+            assertEquals(movies, movieViewModel.popularMovies.value)
+        }
+    }
+}
