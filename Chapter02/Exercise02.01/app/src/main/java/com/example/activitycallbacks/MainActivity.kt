c8cea611ca7676da967a8de6c81f92a4cfef4d4a
Alex Forrester <alex.forrester3@sky.uk>
2024-08-06 21:38:12 +0100

    Adds 2 exercises for Chapter 2

---
+++ Chapter02/Exercise02.01/app/src/main/java/com/example/activitycallbacks/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.activitycallbacks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.activitycallbacks.ui.theme.ActivityCallbacksTheme
+import android.util.Log
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ActivityCallbacksTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        Log.d(TAG, "onCreate")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ActivityCallbacksTheme {
+        Greeting("Android")
+    }
+}
