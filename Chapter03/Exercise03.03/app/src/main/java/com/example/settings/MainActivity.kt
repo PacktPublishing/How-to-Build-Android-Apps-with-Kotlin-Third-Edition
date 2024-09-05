d82c854ee0f77d5c721a2887e291e539a5b7f11f
alexforrester <aforresterwork@gmail.com>
2024-09-05 20:53:10 +0100

    Adds first version of components screen

--- Chapter03/Exercise03.03/app/src/main/java/com/example/settings/MainActivity.kt
+++ Chapter03/Exercise03.03/app/src/main/java/com/example/settings/MainActivity.kt
@@ -4,23 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.settings.ui.theme.HeaderTextStyle
 import com.example.settings.ui.theme.SettingsTheme
 
@@ -31,7 +47,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SettingsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SettingsHeader()
+                    SettingsContainer(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -39,34 +55,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SettingsContainer(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SettingsTheme {
-        Greeting("Android")
+    ) {
+        // Header
+        SettingsHeader()
+        // Image
+        SettingsImage()
+        // Consent cookies
+        SettingsCheckbox()
+        // Stream over wifi
+        SettingsSwitch()
+        // App Brightness
+        SettingsSlider()
+        // Progress bars
+        SettingsProgressBars()
+        // Sign Out Button
+        SignOutButton()
     }
 }
 
 @Composable
 fun SettingsHeader() {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 14.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
-
     ) {
         Text(
             text = stringResource(id = R.string.app_name),
             style = HeaderTextStyle,
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(end = 12.dp),
+            modifier = Modifier.padding(start = 16.dp, end = 12.dp),
         )
         Icon(
             imageVector = Icons.Default.Settings,
@@ -76,9 +101,138 @@ fun SettingsHeader() {
     }
 }
 
+@Composable
+fun SettingsImage() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(id = R.string.settings_profile_image),
+            modifier = Modifier.padding(start = 16.dp),
+            fontSize = 18.sp,
+        )
+        Image(
+            painter = painterResource(id = android.R.drawable.ic_menu_camera),
+            contentDescription = stringResource(id = R.string.settings_profile_image),
+            modifier = Modifier.padding(end = 6.dp)
+        )
+    }
+}
+
+@Composable
+fun SettingsCheckbox() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = stringResource(id = R.string.settings_consent),
+            modifier = Modifier.padding(start = 16.dp),
+            fontSize = 18.sp,
+        )
+        Checkbox(
+            checked = false,
+            onCheckedChange = {},
+            modifier = Modifier.padding(start = 100.dp, end = 4.dp)
+        )
+    }
+}
+
+@Composable
+fun SettingsSwitch() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = stringResource(id = R.string.settings_mobile_streaming),
+            modifier = Modifier.padding(start = 16.dp),
+            fontSize = 18.sp,
+        )
+        Switch(
+            checked = false,
+            onCheckedChange = {},
+            modifier = Modifier.padding(start = 100.dp, end = 12.dp)
+        )
+    }
+}
+
+@Composable
+fun SettingsSlider() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = stringResource(id = R.string.settings_brightness),
+            modifier = Modifier.padding(start = 16.dp),
+            fontSize = 18.sp,
+        )
+        Slider(value = 0f, onValueChange = {}, modifier = Modifier.padding(start = 100.dp, end = 6.dp))
+    }
+}
+
+@Composable
+fun SettingsProgressBars() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        // Spinner Progress Bar
+        CircularProgressIndicator(
+            modifier = Modifier.padding(16.dp)
+        )
+        // Linear Progress Bar
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun SignOutButton() {
+    var showDialog by remember { mutableStateOf(false) }
+
+    Button(onClick = { showDialog = true }) {
+        Text(text = stringResource(id = R.string.show_alert_dialog))
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = stringResource(id = R.string.alert_title)) },
+            text = { Text(text = stringResource(id = R.string.alert_message)) },
+            confirmButton = {
+                Button(onClick = { showDialog = false }) {
+                    Text(text = stringResource(id = R.string.ok))
+                }
+            },
+            dismissButton = {
+                Button(onClick = { showDialog = false }) {
+                    Text(text = stringResource(id = R.string.cancel))
+                }
+            }
+        )
+    }
+}
 
 @Preview
 @Composable
-fun SettingsTitlePreview() {
-    SettingsHeader()
+fun SettingsContainerPreview() {
+    SettingsContainer()
 }
