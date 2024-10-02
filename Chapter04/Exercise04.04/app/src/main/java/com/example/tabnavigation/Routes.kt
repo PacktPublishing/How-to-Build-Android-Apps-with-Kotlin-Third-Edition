b415aea5b7ae917b6791d956ffac4f42bfcd8532
alexforrester <aforresterwork@gmail.com>
2024-10-02 19:42:41 +0100

    Adds chapter 4 exercises and Activity

---
+++ Chapter04/Exercise04.04/app/src/main/java/com/example/tabnavigation/Routes.kt
@@ -0,0 +1,14 @@
+package com.example.tabnavigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.vector.ImageVector
+
+// Define the Navigation Items
+sealed class NavigationItem(var route: String, var icon: ImageVector, var title: String) {
+    data object Home : NavigationItem("home", Icons.Default.Home, "Home")
+    data object Shopping : NavigationItem("recent", Icons.Default.ShoppingCart, "Cart")
+    data object Favorites : NavigationItem("favorites", Icons.Default.Favorite, "Favorites")
+    data object Calendar : NavigationItem("archive", Icons.Default.DateRange, "Calendar")
+    data object Bin : NavigationItem("bin", Icons.Default.Delete, "Bin")
+}
