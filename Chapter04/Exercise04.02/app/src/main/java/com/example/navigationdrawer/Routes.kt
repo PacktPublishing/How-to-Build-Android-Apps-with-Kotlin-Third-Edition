0742d2ac31f099fb659f24385512fd09325a23fb
alexforrester <aforresterwork@gmail.com>
2024-10-06 20:04:03 +0100

    Updates Exercise 4.02

--- Chapter04/Exercise04.02/app/src/main/java/com/example/navigationdrawer/Routes.kt
+++ Chapter04/Exercise04.02/app/src/main/java/com/example/navigationdrawer/Routes.kt
@@ -2,10 +2,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
 
-// Define the Navigation Items
 sealed class NavigationItem(var route: String, var icon: ImageVector, var title: String) {
     object Home : NavigationItem("home", Icons.Default.Home, "Home")
-    object Shopping : NavigationItem("cart", Icons.Default.ShoppingCart, "Cart")
+    object Shopping : NavigationItem("shopping", Icons.Default.ShoppingCart, "Shopping Cart")
     object Favorites : NavigationItem("favorites", Icons.Default.Favorite, "Favorites")
     object Calendar : NavigationItem("calendar", Icons.Default.DateRange, "Calendar")
     object Bin : NavigationItem("bin", Icons.Default.Delete, "Bin")
